86dc5b84bc6cd926d373c581d5c8775935dd8cc8
kyhienhlanh <kyhienlanh@gmail.com>
2023-10-15 13:35:20 +0700

    15/10

--- SachOnline/Areas/Admin/Controllers/homeController.cs
+++ SachOnline/Areas/Admin/Controllers/homeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SachOnline.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,6 +10,7 @@ namespace SachOnline.Areas.Admin.Controllers
     public class homeController : Controller
     {
         // GET: Admin/home
+        private BookOnlineEntities db=new BookOnlineEntities();
         public ActionResult Index()
         {
             return View();
@@ -16,6 +18,25 @@ namespace SachOnline.Areas.Admin.Controllers
         [HttpGet]
         public ActionResult Login()
         {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(FormCollection f)
+        {
+            //Gán các giá trị người dùng nhập liệu cho các biến
+            var sTenDN = f["username"];
+            var sMatKhau = f["password"];
+            //Gán giá trị cho đối tượng được tạo mới (ad)
+            ADMIN ad = db.ADMINs.SingleOrDefault(n => n.TenDN == sTenDN && n.MatKhau== sMatKhau);
+            if (ad != null)
+            {
+                Session["Admin"] = ad;
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ViewBag.ThongBao = "Đăng nhập thất bại";
+            }
             return View();
         }
     }
